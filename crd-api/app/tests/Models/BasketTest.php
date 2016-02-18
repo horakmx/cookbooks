75af8958ff8c64921a1d068982482b1e3780c0b7
Jean-Marc Pulvar <jm.pulvar@125-UX.local>
2016-02-18 21:01:05 +0000

    updated tests structure

---
+++ crd-api/app/tests/Models/BasketTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CrdApi\Tests\Models;
+use \CrdApi\Models\Basket; 
+
+class BasketTest extends \PHPUnit_Framework_TestCase
+{
+    public function testProductcanBeAddedtoBasket()
+    {
+	$basket = new Basket();
+	$this->assertInstanceOf('\CrdApi\Models\Basket', $basket);
+    }
+}
+
+?>

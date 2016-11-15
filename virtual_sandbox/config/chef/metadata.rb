60d6522f7643fc97492134ab1f18794e7f119769
Jean-Marc <jm.pulvar@gmail.com>
2016-11-15 11:18:41 +0000

    Solving the problem with php-fpm cookbook not working properly with Php7 on Ubuntu 16

--- virtual_sandbox/config/chef/metadata.rb
+++ virtual_sandbox/config/chef/metadata.rb
@@ -3,5 +3,6 @@ name "VPS sandbox"
 depends 'vim', '~> 1.1.2'
 depends 'apache2', '~> 3.2.2'
 depends 'php', '~> 2.1.1'
-depends 'php-fpm', '~> 0.7.6'
-depends 'curl', '~> 2.0.3'
+#depends 'php-fpm', '~> 0.7.6'
+depends 'php-fpm'
+depends'curl', '~> 2.0.3'

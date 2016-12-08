0064539a3d3fcb9f7c076c2080b9338cf011506a
Jean-Marc <jean-marc.pulvar@dixonscarphone.com>
2016-12-08 16:45:34 +0000

    Updating dependencies

--- virtual_sandbox/config/chef/metadata.rb
+++ virtual_sandbox/config/chef/metadata.rb
@@ -1,7 +1,13 @@
-name "VPS sandbox"
+name 'VPS sandbox'
 
 depends 'vim', '~> 1.1.2'
 depends 'apache2', '~> 3.2.2'
 depends 'php', '~> 2.1.1'
-depends 'php-fpm', '~> 0.7.6'
+depends 'php-fpm'
 depends 'curl', '~> 2.0.3'
+depends 'mysql', '~> 8.1.1'
+depends 'database', '~> 6.1.1'
+depends 'mysql2_chef_gem', '~> 1.1.0'
+depends 'java', '~>1.42.0'
+depends 'elasticsearch', '~> 3.0.1'
+depends 'kibana', '~> 0.2.1'
